5870e6ae11d478544bcff182317b73d622effcc4
Jonathan Khoo <JONATHANKHOO.22@ichat.sp.edu.sg>
2023-04-24 16:37:18 +0800

    lab2 up

--- Lab2.py
+++ Lab2.py
@@ -1,11 +1,41 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
 
 
-def calculate_bmi(height, weight):
- print("Height = " + str(height))
- print("Weight = " + str(weight))
- bmi = weight / (height * height)
- print("BMI = " + str(bmi))
+#def calculate_bmi(height, weight):
+ #print("Height = " + str(height))
+# print("Weight = " + str(weight))
+#print("Weight = " + str(weight))
+ #bmi = weight / (height * height)
+ #print("BMI = " + str(bmi))
+
+#£££calculate_bmi(height=1.73, weight=57)
+#print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
 
 
-calculate_bmi(height=1.73, weight=57)
+#def calculate_bmi(height, weight):
+# bmi = weight / (height * height)
+ #return bmi
+
+
+# Get user input for height and weight
+#height = float(input("Enter height in meters: "))
+#weight = float(input("Enter weight in kilograms: "))
+
+# Call the calculate_bmi function with user input
+#bmi = calculate_bmi(height, weight)temperature=int(input("temperature :"))sectemp=int(input("temperature :"))wetemp=int(input("temperature :"))print((temperature+sectemp+wetemp)/3)
+
+
+# Print the calculated BMI
+#print("BMI: {:.2f}".format(bmi))
+
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+ def get_user_input():
+def display_main_menu():
+    num_list = get_user_input()
+
+
+
+if __name__ == "__main__":
+main()
